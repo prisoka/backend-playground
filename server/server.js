7af7003943c456ca1c5e041055bfd0ab74e439cc
Priscilla Okawa <prisoka@gmail.com>
2018-06-13 23:08:13 -0700

    got post method to create pet working

--- server/server.js
+++ server/server.js
@@ -8,10 +8,10 @@ const uuid = require('node-uuid');
 //SERVER STARTUP:
 // 1.db configuration with properties (from pg docs)
 const dbClient = new Client({
-  user: '', // created with psql
+  user: 'serverapp', // created with psql
   host: 'localhost', //url
   database: 'pets', // db name
-  password: '', // created with psql
+  password: 'edioh39034pjo^*&0JLKNMIF', // created with psql
   port: 5432,
 })
 dbClient.connect();
@@ -19,7 +19,7 @@ dbClient.connect();
 // parsing request bodies as json:
 app.use(express.json());
 
-// allowing cors for all origins (<<<FIX ME!>>>)
+// allowing cors for all origins (<<<NOT SAFE!!! FIX ME!!!>>> worked for postman, but not client-side)
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -28,13 +28,29 @@ app.use((req, res, next) => {
 
 // setting routes
 // 1. validate data(create) >> create data >> save data (store)
-app.post('/pets', (request, response) => {
+app.post('/pets', (req, res) => {
+  let newPet = {
+    id: uuid.v4(),
+    owner: req.body.ownerId,
+    petName: req.body.petName,
+    petType: req.body.petType
+  }
 
+  let query = 'INSERT INTO pets (id, owner, petName, petType) VALUES ($1, $2, $3, $4)';
+  let values = [newPet.id, newPet.owner, newPet.petName, newPet.petType]
+  dbClient.query(query, values, (err, result) => {
+    if (err) {
+      console.log(err)
+      res.sendStatus(500);
+    } else {
+      res.status(201).json(newPet); // 201: created
+    }
+  })
 })
 
 // get all pets
-app.get('/', (request, response) => {
-
+app.get('/pets', (request, response) => {
+  response.json({name: 'dog'})
 })
 
 // get single pet (id)
